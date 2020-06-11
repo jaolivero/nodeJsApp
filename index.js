baa82da8c8713737a6e46ef43412e21e42d55686
Joel Olivero <joelolivero@outlook.com>
2020-06-10 22:40:08 -0400

    adding third party middleware

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const morgan = require("morgan");
+const helmet = require("helmet");
 const Joi = require("joi");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ xtended: true }));
+app.use(express.static("public"));
+app.use(helmet());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("hello world");
